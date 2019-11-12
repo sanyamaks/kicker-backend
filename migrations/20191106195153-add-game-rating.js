3a24ee854abba79b52934d0092a755ca01fb03a7
Aleksey Kuznetsov <a.kuznetsov@mercurydevelopment.com>
2019-11-12 16:21:54 +0400

    Remove missed console.log

--- migrations/20191106195153-add-game-rating.js
+++ migrations/20191106195153-add-game-rating.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    console.log(Sequelize.INTEGER)
     return queryInterface.createTable("GameRatings", {
       id: {
         allowNull: false,
